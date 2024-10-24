5f74c079193170ae5407a49eaf6ef5db575f38bd
Thinh Nguyen <thinhnguyen.teamx@gmail.com>
2024-10-24 14:10:12 +0700

    update s3 configuration

--- config/plugins.ts
+++ config/plugins.ts
@@ -10,18 +10,20 @@ export default ({ env }) => {
   });
 
   const awsConfig = {
-    accessKeyId: env("AWS_ACCESS_KEY_ID"),
-    secretAccessKey: env("AWS_ACCESS_SECRET"),
-    region: env("AWS_REGION"),
-    params: {
-      ACL: env("AWS_ACL", "public-read"),
-      signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
-      Bucket: env("AWS_BUCKET"),
+    s3Options: {
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: env("AWS_REGION"),
+      params: {
+        ACL: env("AWS_ACL", "public-read"),
+        signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+        Bucket: env("AWS_BUCKET"),
+      },
     },
   };
 
   // Check if required AWS credentials are present
-  if (!awsConfig.accessKeyId || !awsConfig.secretAccessKey || !awsConfig.region || !awsConfig.params.Bucket) {
+  if (!awsConfig.s3Options.accessKeyId || !awsConfig.s3Options.secretAccessKey || !awsConfig.s3Options.region || !awsConfig.s3Options.params.Bucket) {
     console.error('Missing required AWS credentials. Please check your environment variables.');
   }
 
